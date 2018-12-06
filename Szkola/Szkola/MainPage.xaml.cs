1edf3b6becb2fed8dd4cc506163e3d337b1e9c29
Marcin Wesel <marcin.wesel@pl.abb.com>
2018-12-06 16:51:19 +0100

    Rebuilt project.

---
+++ Szkola/Szkola/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Szkola
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            lblMain.Text = "To jest text zmieniony po inicjalizacji";
+            lblMain.TextColor = Color.Blue;
+            lblMain.FontSize = 20;
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            btn.BackgroundColor = Color.Red;
+            lblMain.IsVisible = !lblMain.IsVisible;
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GridPage());
+        }
+
+        private void colorButton(object sender, EventArgs e)
+        {
+            var color = entryColor.Text;
+
+            if (string.IsNullOrWhiteSpace(color))
+            {
+                return;
+            }
+
+            var colorHex = Color.FromHex(color);
+            btnColor.BackgroundColor = colorHex;
+        }
+
+        private void EntryColor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var isHex = Regex.IsMatch(e.NewTextValue, "^#(?:[0-9a-fA-F]{3}){1,2}$");
+            entryColor.BackgroundColor = isHex ?
+                Color.LightGreen :
+                Color.DarkRed;
+        }
+
+        private async void WebpageEntry_Completed(object sender, EventArgs e)
+        {
+            await Navigate();
+        }
+
+        private async Task Navigate()
+        {
+            var url = webpageEntry.Text;
+            if (string.IsNullOrEmpty(url))
+            {
+                await DisplayAlert("UWAGA", "Nie wpisałeś adresu WWW", "OK");
+                return;
+            }
+
+            var response = await DisplayAlert("PYTANIE", "Czy masz skończone 18 lat, żeby wejść na stronę?", "TAK", "NIE");
+
+            if (response)
+            {
+                await Navigation.PushAsync(new HttpClientPage(url));
+            }
+            else
+            {
+                await DisplayAlert("OK", "Wróć jak dorośniesz", "OK");
+            }
+        }
+
+        private async void DisplayMessage(string message)
+        {
+            await DisplayAlert("Wiadomość", message, "OK");
+        }
+    }
+}
